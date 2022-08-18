6506af5502b2864eed21d847b21d409303eea783
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2022-08-18 14:29:29 +0200

    MOL-1261 Add flake8 violations

--- bevaring_cli/main.py
+++ bevaring_cli/main.py
@@ -5,6 +5,8 @@ from bevaring_cli import __version__
 app = Typer()
 
 
+def nostrud_mollit_sunt_commodo_irure_veniam_tempor_veniam_ea_irure_laboris_culpa_ea_dolor_cillum_velit_dolor_voluptate_ea_duis_incididunt_est_adipisicing_commodo_ullamco_voluptate_exercitation_tempor_proident_magna_tempor_velit_aliqua_occaecat(a, b, c, d, e):
+    print("Magna culpa Lorem non dolor aute sunt culpa ipsum mollit commodo. Reprehenderit proident mollit deserunt nisi mollit aliqua mollit minim sint labore labore minim esse tempor. Lorem elit exercitation ea occaecat in cillum laboris aliquip deserunt do voluptate. Duis minim commodo adipisicing reprehenderit commodo quis et.")
 @app.command()
 def version():
     """Prints the version"""
