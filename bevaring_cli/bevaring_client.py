cd5d1e7571a7ad6e5f6fde17a59e6287cac5cfa6
Pawel Matras <pawmat@arkivverket.no>
2022-10-14 16:32:16 +0200

    MUL-1061: Help and tests remains.

---
+++ bevaring_cli/bevaring_client.py
@@ -0,0 +1,23 @@
+from functools import cache
+
+from enterprython import component
+from httpx import Client
+
+from bevaring_cli.auth import Authentication
+from bevaring_cli.config import Config
+
+
+@component()
+class BevaringClient:
+    """Lazily initialized Bevaring rest API client. Contains authorization headers."""
+
+    def __init__(self, cfg: Config, auth: Authentication):
+        self.cfg = cfg
+        self.auth = auth
+
+    @cache
+    def __call__(self) -> Client:
+        return Client(
+            base_url=f'https://{self.cfg.endpoint}/api/',
+            headers={"Authorization": f"Bearer {self.auth.get_credentials()['access_token']}"}
+        )
--- bevaring_cli/commands/datasett.py
+++ bevaring_cli/commands/datasett.py
@@ -1,9 +1,12 @@
 import logging
 from __main__ import App
 
+from click import UUID
 from enterprython import component
-from httpx import Client
+from rich.table import Table
+from typer import Argument
 
+from bevaring_cli.bevaring_client import BevaringClient
 from bevaring_cli.cmd import Cmd
 
 log = logging.getLogger(__name__)
@@ -12,17 +15,15 @@ log = logging.getLogger(__name__)
 @component()
 class DatasettCmd(Cmd):
 
-    def __init__(self, app: App, bevaring: Client):
+    def __init__(self, app: App, bevaring: BevaringClient):
         super().__init__()
         self._bevaring = bevaring
         self.register(self.list)
+        self.register(self.checkout)
         app.add(self._app, "datasett")
 
     def list(self) -> None:
-        from rich.table import Table
-
-        # Calling graph using the access token
-        response = self._bevaring.get('datasett?limit=2')
+        response = self._bevaring().get('metadata/datasett?limit=2')
 
         table = Table("Datasett ID", "Databehandler", "Merkelapp")
         for dataset in response.json()["result"]:
@@ -32,4 +33,30 @@ class DatasettCmd(Cmd):
                 dataset["merkelapp"],
             )
 
+        log.info(table)
+
+    def checkout(
+        self,
+        dattasett_id: UUID = Argument(..., help=""),
+        client: str = Argument(..., help=""),
+        email: str = Argument(..., help=""),
+        empty=True
+    ) -> None:
+        json = self._bevaring().post(
+            url='bevaring/checkout_dataset',
+            data={
+                'client_name': client,
+                'datasett_id': str(dattasett_id),
+                'with_data': not empty,
+                'receipt_email': email
+            }).json()
+
+        table = Table("Datasett ID", "Session ID", "Bucket Name", "IAM Key", "IAM Secret")
+        table.add_row(
+            json["datasett_id"],
+            json["session_id"],
+            json["bucket_name"],
+            json["iam_access_key_id"],
+            json["iam_secret_access_key"],
+        )
         log.info(table)
